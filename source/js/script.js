5f7fe16231a70755a4e0caf697c13db4dd26d7b5
Andrei Vetrov <vettrov78@gmail.com>
2021-08-28 11:03:38 +0300

    Добавил логику нажатия гамбургера

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,16 @@
-var navMain = document.querySelector(".page-header__navigation")
+var navMain = document.querySelector(".page-header__navigation");
+var navToggle = document.querySelector(".page-header__navigation-toggle");
 
-navMain.classList.remove("page-header__nojs")
+navMain.classList.remove("page-header--nojs");
+navMain.classList.add("page-header__navigation--closed");
+navMain.classList.remove("page-header__navigation--opened");
+
+navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains("page-header__navigation--opened")) {
+        navMain.classList.add("page-header__navigation--closed");
+        navMain.classList.remove("page-header__navigation--opened");
+    } else {
+        navMain.classList.remove("page-header__navigation--closed");
+        navMain.classList.add("page-header__navigation--opened");
+    }
+})
